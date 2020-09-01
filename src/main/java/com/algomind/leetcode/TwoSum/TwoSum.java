abd12974421f7b8114769e8458719705a0097671
Kefeng Zhang <kefeng.zhang@algomind.co.uk>
2020-09-02 00:17:44 +0100

    Initial commit

---
+++ src/main/java/com/algomind/leetcode/TwoSum/TwoSum.java
@@ -0,0 +1,21 @@
+package com.algomind.leetcode.TwoSum;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0 ; i < nums.length ; i++) {
+            int want = target - nums[i];
+            if(map.containsKey(want)) {
+                return new int[]{map.get(want), i};
+            }
+
+            map.put(nums[i], i);
+        }
+
+        throw new IllegalArgumentException();
+    }
+}

5bc235924abb55a5d4f3005d6cff7f919787bcb4
Kefeng Zhang <kefeng.zhang@algomind.co.uk>
2020-09-03 13:41:09 +0100

    Added LongestSubstring

---
+++ src/test/java/com/algomind/leetcode/longestsubstring/LongestSubstringTest.java
@@ -0,0 +1,29 @@
+package com.algomind.leetcode.longestsubstring;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+class LongestSubstringTest {
+    private LongestSubstring longestSubstring = new LongestSubstring();
+
+    @ParameterizedTest
+    @MethodSource("provideData")
+    public void test1(int result, String s) {
+        Assertions.assertEquals(result, longestSubstring.lengthOfLongestSubstring(s));
+    }
+
+    private static Stream<Arguments> provideData() {
+        return Stream.of(
+                Arguments.of(0, ""),
+                Arguments.of(1, "a"),
+                Arguments.of(3, "abcabcbb"),
+                Arguments.of(1, "bbbbb"),
+                Arguments.of(3, "pwwkew"),
+                Arguments.of(2, "abbbbbba")
+        );
+    }
+}

3879cfb9ed2cacd867c0cd6ed0b651b070398662
Kefeng Zhang <kzhangtech@gmail.com>
2021-03-15 18:43:59 +0000

    Added FibonacciNumber

---
+++ src/test/java/com/algomind/leetcode/fibonaccinumber/FibonacciNumberTest.java
@@ -0,0 +1,30 @@
+package com.algomind.leetcode.fibonaccinumber;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+class FibonacciNumberTest {
+    private final FibonacciNumber fibonacciNumber = new FibonacciNumber();
+
+    @ParameterizedTest
+    @MethodSource("provideData")
+    public void testFibonacciNumber1(int expected, int input) {
+        Assertions.assertEquals(expected, fibonacciNumber.fib1(input));
+    }
+
+    private static Stream<Arguments> provideData() {
+        return Stream.of(
+                Arguments.of(1, 2),
+                Arguments.of(2, 3),
+                Arguments.of(3, 4),
+                Arguments.of(5, 5),
+                Arguments.of(8, 6),
+                Arguments.of(13, 7),
+                Arguments.of(21, 8)
+        );
+    }
+}

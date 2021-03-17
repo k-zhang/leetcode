6dcc3a3e3012f232ad4163d61dfb9fbd4b1d294b
Kefeng Zhang <kzhangtech@gmail.com>
2021-03-17 00:47:05 +0000

    Added IsSubsequence

---
+++ src/test/java/com/algomind/leetcode/issubsequence/IsSubsequenceTest.java
@@ -0,0 +1,27 @@
+package com.algomind.leetcode.issubsequence;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+class IsSubsequenceTest {
+    private final IsSubsequence isSubsequence = new IsSubsequence();
+
+    @ParameterizedTest
+    @MethodSource("provideData")
+    public void testIsSubsequence(boolean expected, String s, String t) {
+        Assertions.assertEquals(expected, isSubsequence.isSubsequence(s, t));
+    }
+
+    private static Stream<Arguments> provideData() {
+        return Stream.of(
+                Arguments.of(true, "abc", "ahbgdc"),
+                Arguments.of(false, "axc", "ahbgdc"),
+                Arguments.of(true, "", ""),
+                Arguments.of(true, "", "a")
+        );
+    }
+}

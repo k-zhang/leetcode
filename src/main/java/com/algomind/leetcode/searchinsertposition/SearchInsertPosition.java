27037d9b4c8423ef9fe3ee0e521b9de2a386c1fd
Kefeng Zhang <kzhangtech@gmail.com>
2021-03-23 01:12:41 +0000

    Added Search Insert Position

---
+++ src/main/java/com/algomind/leetcode/searchinsertposition/SearchInsertPosition.java
@@ -0,0 +1,31 @@
+package com.algomind.leetcode.searchinsertposition;
+
+public class SearchInsertPosition {
+    public int searchInsert(int[] nums, int target) {
+        if(target < nums[0]) return 0;
+        if(target > nums[nums.length-1]) return nums.length;
+
+        int left = 0, right = nums.length - 1;
+        int result = 0;
+
+        while (left <= right) {
+            int mid = (right - left) / 2 + left;
+            if (target < nums[mid]) {
+                right = mid - 1;
+            } else if (target > nums[mid]) {
+                left = mid + 1;
+            } else {
+                result = mid;
+                break;
+            }
+        }
+
+        if (target < nums[left]) {
+            result = left;
+        } else if (target > nums[right]) {
+            result = right;
+        }
+
+        return result;
+    }
+}

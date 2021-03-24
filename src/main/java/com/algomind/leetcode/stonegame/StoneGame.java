1e37ecdc4e54115c65f91587b70be1482e5f3b4b
Kefeng Zhang <kzhangtech@gmail.com>
2021-03-24 19:52:21 +0000

    Added Stone Game

---
+++ src/main/java/com/algomind/leetcode/stonegame/StoneGame.java
@@ -0,0 +1,19 @@
+package com.algomind.leetcode.stonegame;
+
+public class StoneGame {
+    public boolean stoneGame(int[] piles) {
+        int[][] dp = new int[piles.length][piles.length];
+
+        for(int i = 0 ; i < piles.length; i++) {
+            dp[i][i] = piles[i];
+        }
+
+        for(int i = piles.length - 2; i >= 0 ; i--) {
+            for(int j = i + 1 ; j <= piles.length - 1 ; j++) {
+                dp[i][j] = Math.max(piles[i] - dp[i+1][j], piles[j] - dp[i][j-1]);
+            }
+        }
+
+        return dp[0][piles.length-1] > 0;
+    }
+}

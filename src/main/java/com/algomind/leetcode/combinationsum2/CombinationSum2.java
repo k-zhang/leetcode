f357828447a4afb82b6c582f1ba848c94f171e31
Kefeng Zhang <kzhangtech@gmail.com>
2021-04-06 17:33:11 +0100

    Added Combination Sum II

---
+++ src/main/java/com/algomind/leetcode/combinationsum2/CombinationSum2.java
@@ -0,0 +1,45 @@
+package com.algomind.leetcode.combinationsum2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class CombinationSum2 {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        var result = new ArrayList<List<Integer>>();
+        var path = new LinkedList<Integer>();
+
+        Arrays.sort(candidates);
+        recursion(candidates, 0, target, result, path);
+
+        return result;
+    }
+
+    private void recursion(int[] candidate, int index, int remain, List<List<Integer>> result, LinkedList<Integer> path) {
+        if(remain == 0) {
+            result.add(new ArrayList<>(path));
+            System.out.println("Selected:" + path);
+            return;
+        }
+
+        int lastPop = -1;
+        for(int i = index ; i < candidate.length && remain >= candidate[i] ; i ++) {
+            if(path.isEmpty() && i - 1 >= 0 && candidate[i] == candidate[i-1]) {
+                System.out.println("Jump off A:" + candidate[i]);
+                continue;
+            }
+
+            if(lastPop == candidate[i]) {
+                System.out.println("Jump off B:" + candidate[i]);
+                continue;
+            }
+
+            path.add(candidate[i]);
+            System.out.println("Path (Add " + candidate[i] + ")   :" + path);
+            recursion(candidate, i+1, remain - candidate[i], result, path);
+            lastPop = path.removeLast();
+            System.out.println("Path (Remove " + lastPop + "):" + path);
+        }
+    }
+}
---
+++ src/test/java/com/algomind/leetcode/combinationsum2/CombinationSum2Test.java
@@ -0,0 +1,45 @@
+package com.algomind.leetcode.combinationsum2;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+class CombinationSum2Test {
+    private final CombinationSum2 combinationSum2 = new CombinationSum2();
+
+    @ParameterizedTest
+    @MethodSource("provideData")
+    public void testThreeSum(List<List<Integer>> expected, int[] nums, int target) {
+        List<List<Integer>> result = combinationSum2.combinationSum2(nums, target);
+        Assertions.assertEquals(expected.size(), result.size());
+
+        int matched = 0;
+        for (List<Integer> threeNumbers : expected) {
+            for (int j = 0; j < result.size(); j++) {
+                List<Integer> resultThreeNumbers = expected.get(j);
+                if (threeNumbers.containsAll(resultThreeNumbers) && resultThreeNumbers.containsAll(threeNumbers)) {
+                    matched++;
+                }
+            }
+        }
+
+        Assertions.assertEquals(expected.size(), matched);
+    }
+
+    private static Stream<Arguments> provideData() {
+        return Stream.of(
+            Arguments.of(List.of(List.of(1,1,6),List.of(1,2,5),List.of(1,7),List.of(2,6)), new int[]{10,1,2,7,6,1,5}, 8),
+            Arguments.of(List.of(List.of(1,2,2),List.of(5)), new int[]{2,5,2,1,2}, 5),
+            Arguments.of(List.of(List.of(1,1,1,1,1)), new int[]{1,1,1,1,1}, 5),
+            Arguments.of(List.of(List.of(1,1,1,2)), new int[]{1,1,1,1,2}, 5),
+            Arguments.of(List.of(List.of(1,1,1,2),List.of(1,1,1,1,1)), new int[]{1,1,1,1,1,2}, 5),
+            Arguments.of(List.of(), new int[]{2,2,2,2}, 5),
+            Arguments.of(List.of(List.of(2,2,3,3),List.of(1,1,2,3,3),List.of(10)), new int[]{1,1,2,2,3,3,10}, 10),
+            Arguments.of(List.of(List.of(5,5),List.of(2,2,2,2,2)), new int[]{5,5,5,2,2,5,2,5,2,2}, 10)
+        );
+    }
+}

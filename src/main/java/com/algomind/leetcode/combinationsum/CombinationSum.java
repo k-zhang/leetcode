8718c07bcef37f4e6c54633adc1c0ca304dc01b5
Kefeng Zhang <kzhangtech@gmail.com>
2021-04-06 14:54:11 +0100

    Added Combination Sum

---
+++ src/main/java/com/algomind/leetcode/combinationsum/CombinationSum.java
@@ -0,0 +1,30 @@
+package com.algomind.leetcode.combinationsum;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class CombinationSum {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        var result = new ArrayList<List<Integer>>();
+        var potentialResult = new LinkedList<Integer>();
+
+        recursion(candidates, 0, target, result, potentialResult);
+
+        return result;
+    }
+
+    private void recursion(int[] candidate, int index, int remain, List<List<Integer>> result, LinkedList<Integer> potentialResult) {
+        if(remain < 0) return;
+        if(remain == 0) {
+            result.add(new ArrayList<>(potentialResult));
+            return;
+        }
+
+        for(int i = index; i < candidate.length ; i++) {
+            potentialResult.add(candidate[i]);
+            recursion(candidate, i, remain - candidate[i], result, potentialResult);
+            potentialResult.removeLast();
+        }
+    }
+}

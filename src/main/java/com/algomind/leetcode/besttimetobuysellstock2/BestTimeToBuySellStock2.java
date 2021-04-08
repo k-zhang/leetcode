363568a3baf015559266b74a06453c2a492aa729
Kefeng Zhang <kzhangtech@gmail.com>
2021-04-08 20:29:24 +0100

    Added Best Time to Buy and Sell Stock II

---
+++ src/main/java/com/algomind/leetcode/besttimetobuysellstock2/BestTimeToBuySellStock2.java
@@ -0,0 +1,24 @@
+package com.algomind.leetcode.besttimetobuysellstock2;
+
+public class BestTimeToBuySellStock2 {
+    public int maxProfit(int[] prices) {
+        int i = 0 ;
+        int totalProfit = 0;
+
+        while(i < prices.length - 1) {
+            int maxProfit = 0;
+            int j = i + 1;
+            while(j <= prices.length - 1 && prices[i] < prices[j]) {
+                int potentialProfit = prices[j] - prices[i];
+                if(potentialProfit <= maxProfit) break;
+                maxProfit = potentialProfit;
+                j++;
+            }
+
+            totalProfit += maxProfit;
+            i = j;
+        }
+
+        return totalProfit;
+    }
+}

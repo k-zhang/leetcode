8b0548050b7975d423ad325db814f8e95dfdf5fe
Kefeng Zhang <kzhangtech@gmail.com>
2021-04-13 13:58:54 +0100

    Added Binary Tree Inorder Traversal

---
+++ src/test/java/com/algomind/leetcode/binarytreeinordertraversal/BinaryTreeInOrderTraversalTest.java
@@ -0,0 +1,30 @@
+package com.algomind.leetcode.binarytreeinordertraversal;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+class BinaryTreeInOrderTraversalTest {
+    private final BinaryTreeInOrderTraversal binaryTreeInOrderTraversal = new BinaryTreeInOrderTraversal();
+
+    @ParameterizedTest
+    @MethodSource("provideData")
+    public void testCreateBinaryTreeInOrder(Integer[] expected, Integer[] nums) {
+        TreeNode tree = TreeCreation.constructTree(nums);
+        List<Integer> result = binaryTreeInOrderTraversal.inorderTraversal(tree);
+
+        Assertions.assertArrayEquals(expected, result.toArray(new Integer[0]));
+    }
+
+    private static Stream<Arguments> provideData() {
+        return Stream.of(
+                Arguments.of(new Integer[]{8,4,2,5,1,6,3,7}, new Integer[]{1,2,3,4,5,6,7,8}),
+                Arguments.of(new Integer[]{1,6,3,7,2,8,5}, new Integer[]{1,null,2,3,5,6,7,8}),
+                Arguments.of(new Integer[]{1,6,8,3,7,2}, new Integer[]{1,null,2,3,null,6,7,null,8})
+        );
+    }
+}
---
+++ src/test/java/com/algomind/leetcode/binarytreeinordertraversal/TreeCreation.java
@@ -0,0 +1,40 @@
+package com.algomind.leetcode.binarytreeinordertraversal;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class TreeCreation {
+    public static TreeNode constructTree(Integer[] nums) {
+        if(nums.length == 0) return null;
+        if(nums.length == 1) {
+            if(nums[0] == null) throw new IllegalArgumentException("Cannot have one single null as input.");
+            return new TreeNode(nums[0]);
+        }
+
+        List<TreeNode> tree = Arrays.stream(nums).map(TreeNode::new).collect(Collectors.toList());
+
+        int index = 0, search = 1;
+        while(search < tree.size()) {
+            TreeNode node = tree.get(index);
+            if(node.val != null) {
+                node.left = tree.get(search++);
+                if(search < tree.size()) node.right = tree.get(search++);
+            }
+            index ++;
+        }
+
+        cleanNull(tree.get(0));
+
+        return tree.get(0);
+    }
+
+    private static void cleanNull(TreeNode treeNode) {
+        if(treeNode == null) return;
+
+        if(treeNode.left != null && treeNode.left.val == null) treeNode.left = null;
+        if(treeNode.right != null && treeNode.right.val == null) treeNode.right = null;
+
+        if(treeNode.left != null) cleanNull(treeNode.left);
+        if(treeNode.right != null) cleanNull(treeNode.right);
+    }
+}

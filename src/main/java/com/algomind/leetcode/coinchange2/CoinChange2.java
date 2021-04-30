89b226c0628db7be2ff915cb6e8a7c9a574fedb7
Kefeng Zhang <kzhangtech@gmail.com>
2021-04-30 17:28:29 +0100

    Added Coin Change 2

---
+++ src/main/java/com/algomind/leetcode/coinchange2/CoinChange2.java
@@ -0,0 +1,16 @@
+package com.algomind.leetcode.coinchange2;
+
+public class CoinChange2 {
+    public int change(int amount, int[] coins) {
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+
+        for(int coin : coins) {
+            for(int i = coin ; i <= amount ; i ++) {
+                dp[i] = dp[i] + dp[i - coin];
+            }
+        }
+
+        return dp[amount];
+    }
+}
---
+++ src/test/java/com/algomind/leetcode/coinchange2/CoinChange2Test.java
@@ -0,0 +1,28 @@
+package com.algomind.leetcode.coinchange2;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+class CoinChange2Test {
+    private final CoinChange2 coinChange2 = new CoinChange2();
+
+    @ParameterizedTest
+    @MethodSource("provideData")
+    public void test(int expected, int amount, int[] coins) {
+        Assertions.assertEquals(expected, coinChange2.change(amount, coins));
+    }
+
+    private static Stream<Arguments> provideData() {
+        return Stream.of(
+                Arguments.of(4, 5, new int[]{1,2,5}),
+                Arguments.of(0, 3, new int[]{2}),
+                Arguments.of(1, 10, new int[]{10}),
+                Arguments.of(3119, 200, new int[]{1,3,5,50,100}),
+                Arguments.of(1, 0, new int[]{1,3,5,50,100})
+        );
+    }
+}

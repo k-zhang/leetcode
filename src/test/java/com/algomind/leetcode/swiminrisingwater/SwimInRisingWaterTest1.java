4f1d548c1ec0655ef1c34a4ca1ed8316950b3f35
Kefeng Zhang <kzhangtech@gmail.com>
2021-06-20 22:44:54 +0100

    Added Swim in Rising Water

---
+++ src/test/java/com/algomind/leetcode/swiminrisingwater/SwimInRisingWaterTest1.java
@@ -0,0 +1,25 @@
+package com.algomind.leetcode.swiminrisingwater;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+class SwimInRisingWaterTest1 {
+    private final SwimInRisingWater1 solution = new SwimInRisingWater1();
+
+    @ParameterizedTest
+    @MethodSource("provideData")
+    public void test(int expected, int[][] grid) {
+        Assertions.assertEquals(expected, solution.swimInWater(grid));
+    }
+
+    private static Stream<Arguments> provideData() {
+        return Stream.of(
+                Arguments.of(3, new int[][]{{0,2},{1,3}}),
+                Arguments.of(16, new int[][]{{0,1,2,3,4},{24,23,22,21,5},{12,13,14,15,16},{11,17,18,19,20},{10,9,8,7,6}})
+        );
+    }
+}

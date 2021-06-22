458c7adbb3c8d5af1d8f01ea1d6ee2cdd36f8926
Kefeng Zhang <kzhangtech@gmail.com>
2021-06-22 14:15:12 +0100

    Slight performance improvement for All Paths from Source Lead to Destination

--- src/main/java/com/algomind/leetcode/allpathsfromsourceleadtodestination/AllPathsFromSourceLeadToDestination.java
+++ src/main/java/com/algomind/leetcode/allpathsfromsourceleadtodestination/AllPathsFromSourceLeadToDestination.java
@@ -7,17 +7,17 @@ public class AllPathsFromSourceLeadToDestination {
     public boolean leadsToDestination(int n, int[][] edges, int source, int destination) {
         var pathMap = Arrays.stream(edges).collect(Collectors.groupingBy(edge -> edge[0]));
 
-        Set<Integer> tobeExamined = new HashSet<>();
-        tobeExamined.add(source);
-
+        int[] nextSteps = new int[]{source};
         Set<Integer> visited = new HashSet<>();
 
-        return doSearch(pathMap, tobeExamined, visited, destination);
+        return doSearch(pathMap, nextSteps, visited, destination);
     }
 
-    private boolean doSearch(Map<Integer, List<int[]>> pathMap, Set<Integer> tobeExamined, Set<Integer> visited, int destination) {
+    private boolean doSearch(Map<Integer, List<int[]>> pathMap, int[] tobeExamined, Set<Integer> visited, int destination) {
         boolean result = true;
         for(Integer node : tobeExamined) {
+            if(visited.contains(node)) return false;
+
             List<int[]> paths = pathMap.get(node);
             if(paths == null) {
                 result &= node == destination;
@@ -25,22 +25,14 @@ public class AllPathsFromSourceLeadToDestination {
                 continue;
             }
 
-            Set<Integer> myVisited = new HashSet<>(visited);
-            myVisited.add(node);
+            var nextSteps = paths.stream().mapToInt(path -> path[1]).toArray();
 
-            var nextSteps = paths.stream().map(path -> path[1]).collect(Collectors.toSet());
-            if(containsLoop(nextSteps, visited)) return false;
-
-            result &= doSearch(pathMap, nextSteps, myVisited, destination);
+            visited.add(node);
+            result &= doSearch(pathMap, nextSteps, visited, destination);
             if(!result) return false;
+            visited.remove(node);
         }
 
         return result;
-    }
-
-    private boolean containsLoop(Set<Integer> nextSteps, Set<Integer> visited) {
-        Set<Integer> intersection = new HashSet<>(nextSteps);
-        intersection.retainAll(visited);
-        return !intersection.isEmpty();
     }
 }

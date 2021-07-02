3c283aab21bc4c22d8b3d1bd0863f09db86740c9
Kefeng Zhang <kzhangtech@gmail.com>
2021-07-02 10:21:57 +0100

    Added Find K Closest Elements

---
+++ src/main/java/com/algomind/leetcode/findkclosestelements/FindKClosestElements.java
@@ -0,0 +1,52 @@
+package com.algomind.leetcode.findkclosestelements;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindKClosestElements {
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        int n = arr.length;
+
+        var result = new ArrayList<Integer>();
+
+        int left = -1, right = -1;
+
+        if(x < arr[0] || x > arr[n - 1]) {
+            if (x < arr[0]) {
+                left = 0;
+                right = left + k - 1;
+            } else if (x > arr[n - 1]) {
+                right = n - 1;
+                left = right - k + 1;
+            }
+        }else{
+            for(int i = 0 ; i < n ; i ++) {
+                if(arr[i] == x) {
+                    left = right = i;
+                    break;
+                }else if(arr[i] < x && arr[i + 1] > x) {
+                    if(Math.abs(arr[i] - x) <=  Math.abs(arr[i + 1] - x)) {
+                        left = right = i;
+                    }else{
+                        left = right = i + 1;
+                    }
+                    break;
+                }
+            }
+
+            for(int i = 0 ; i < k - 1 ; i ++) {
+                if(right + 1 == n || (left != 0 && Math.abs(arr[left - 1] - x) <=  Math.abs(arr[right + 1] - x))) {
+                    left = left - 1;
+                }else{
+                    right = right + 1;
+                }
+            }
+        }
+
+        for(int i = left ; i <= right ; i ++) {
+            result.add(arr[i]);
+        }
+
+        return result;
+    }
+}
---
+++ src/test/java/com/algomind/leetcode/findkclosestelements/FindKClosestElementsTest.java
@@ -0,0 +1,42 @@
+package com.algomind.leetcode.findkclosestelements;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+class FindKClosestElementsTest {
+    private final FindKClosestElements solution = new FindKClosestElements();
+
+    @ParameterizedTest
+    @MethodSource("provideData")
+    public void test(int[] expected, int[] arr, int k, int x) {
+        var result = solution.findClosestElements(arr, k, x);
+        int[] resultArray = result.stream().mapToInt(i -> i).toArray();
+        Assertions.assertArrayEquals(expected, resultArray);
+    }
+
+    private static Stream<Arguments> provideData() {
+        return Stream.of(
+            Arguments.of(new int[]{1,2,3,4}, new int[]{1,2,3,4,5}, 4, 3),
+            Arguments.of(new int[]{1,2,3,4}, new int[]{1,2,3,4,5}, 4, -1),
+            Arguments.of(new int[]{2,3,4,5}, new int[]{1,2,3,4,5}, 4, 6),
+            Arguments.of(new int[]{1,2,4,5}, new int[]{1,2,4,5,6}, 4, 3),
+            Arguments.of(new int[]{2,4,5,6}, new int[]{2,4,5,6,7}, 4, 3),
+            Arguments.of(new int[]{5,6,7,9}, new int[]{2,4,5,6,7,9}, 4, 8),
+            Arguments.of(new int[]{3}, new int[]{1,2,3,4,5}, 1, 3),
+            Arguments.of(new int[]{1}, new int[]{1,2,3,4,5}, 1, -1),
+            Arguments.of(new int[]{5}, new int[]{1,2,3,4,5}, 1, 6),
+            Arguments.of(new int[]{2}, new int[]{1,2,4,5,6}, 1, 3),
+            Arguments.of(new int[]{2}, new int[]{2,4,5,6,7}, 1, 3),
+            Arguments.of(new int[]{7}, new int[]{2,4,5,6,7,9}, 1, 8),
+            Arguments.of(new int[]{1}, new int[]{1}, 1, 8),
+            Arguments.of(new int[]{1}, new int[]{1}, 1, -2),
+            Arguments.of(new int[]{1}, new int[]{1}, 1, 1),
+            Arguments.of(new int[]{5}, new int[]{1,5}, 1, 4),
+            Arguments.of(new int[]{5}, new int[]{1,5}, 1, 5)
+        );
+    }
+}

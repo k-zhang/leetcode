f0cbd6efa162120caa9449bb2d842c71b9d3337e
Kefeng Zhang <kzhangtech@gmail.com>
2021-07-19 01:13:59 +0100

    Added another solition to Three Equal Parts

--- src/main/java/com/algomind/leetcode/threeequalparts/ThreeEqualParts1.java
+++ src/main/java/com/algomind/leetcode/threeequalparts/ThreeEqualParts1.java
@@ -1,45 +1,48 @@
 package com.algomind.leetcode.threeequalparts;
 
+import java.util.Arrays;
+
 public class ThreeEqualParts1 {
     public int[] threeEqualParts(int[] arr) {
-        int sum[] = new int[arr.length];
-        sum[0] = arr[0];
-        for(int i = 1; i < arr.length; i++)
-            sum[i] = sum[i-1] + arr[i];
-        if(sum[arr.length - 1] == 0)
-            return new int[]{0, arr.length - 1};
-        int tot = sum[arr.length - 1];
-        if(tot % 3 != 0)
-            return new int[]{-1, -1};
-        int lastZeroCnt = 0;
-        while(arr[arr.length - 1 - lastZeroCnt] == 0)
-            lastZeroCnt++;
-        int pos1 = lowerBound(sum, 0, sum.length, tot / 3) + lastZeroCnt;
-        int pos2 = lowerBound(sum, 0, sum.length, tot * 2 / 3) + lastZeroCnt + 1;
-        int end1 = pos1, end2 = pos2 - 1, end3 = arr.length - 1;
-        while(end1 >= 0 && end2 > pos1 && end3 >= pos2) {
-            if(arr[end1] == arr[end2] && arr[end2] == arr[end3]){
-                end1--;
-                end2--;
-                end3--;
-            } else {
-                return new int[]{-1, -1};
+        int[] IMP = new int[]{-1, -1};
+        int N = arr.length;
+
+        int S = 0;
+        for (int x: arr) S += x;
+        if (S % 3 != 0) return IMP;
+        int T = S / 3;
+        if (T == 0)
+            return new int[]{0, N - 1};
+
+        int i1 = -1, j1 = -1, i2 = -1, j2 = -1, i3 = -1, j3 = -1;
+        int su = 0;
+        for (int i = 0; i < N; ++i) {
+            if (arr[i] == 1) {
+                su += 1;
+                if (su == 1) i1 = i;
+                if (su == T) j1 = i;
+                if (su == T+1) i2 = i;
+                if (su == 2*T) j2 = i;
+                if (su == 2*T + 1) i3 = i;
+                if (su == 3*T) j3 = i;
             }
         }
 
-        if(end1>=0 && sum[end1] > 0) return new int[]{-1, -1};
+        // The array is in the form W [i1, j1] X [i2, j2] Y [i3, j3] Z
+        // where [i1, j1] is a block of 1s, etc.
+        int[] part1 = Arrays.copyOfRange(arr, i1, j1+1);
+        int[] part2 = Arrays.copyOfRange(arr, i2, j2+1);
+        int[] part3 = Arrays.copyOfRange(arr, i3, j3+1);
 
-        return new int[]{pos1, pos2};
-    }
+        if (!Arrays.equals(part1, part2)) return IMP;
+        if (!Arrays.equals(part1, part3)) return IMP;
 
-    private int lowerBound(int[] arr, int begin, int end, int tar) {
-        while(begin < end) {
-            int mid = begin + (end - begin) / 2;
-            if(arr[mid] < tar)
-                begin = mid + 1;
-            else
-                end = mid;
-        }
-        return begin;
+        // x, y, z: the number of zeros after part 1, 2, 3
+        int x = i2 - j1 - 1;
+        int y = i3 - j2 - 1;
+        int z = arr.length - j3 - 1;
+
+        if (x < z || y < z) return IMP;
+        return new int[]{j1+z, j2+z+1};
     }
 }

29240634b64c2771867314f4aa9feb19d77c9394
Kefeng Zhang <kzhangtech@gmail.com>
2021-07-26 10:09:23 +0100

    Added Non-negative Integers without Consecutive Ones

---
+++ src/test/java/com/algomind/leetcode/nonnegativeintegerswithoutconsecutiveones/NonnegativeIntegersWithoutConsecutiveOnesTest.java
@@ -0,0 +1,26 @@
+package com.algomind.leetcode.nonnegativeintegerswithoutconsecutiveones;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+class NonnegativeIntegersWithoutConsecutiveOnesTest {
+    private final NonnegativeIntegersWithoutConsecutiveOnes solution = new NonnegativeIntegersWithoutConsecutiveOnes();
+
+    @ParameterizedTest
+    @MethodSource("provideData")
+    public void test(int expected, int num) {
+        Assertions.assertEquals(expected, solution.findIntegers(num));
+    }
+
+    private static Stream<Arguments> provideData() {
+        return Stream.of(
+            Arguments.of(2, 1),
+            Arguments.of(3, 2),
+            Arguments.of(5, 5)
+        );
+    }
+}

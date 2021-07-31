c975e6461cd6fa0122a5e3034ffafbca1562df64
Kefeng Zhang <kzhangtech@gmail.com>
2021-07-31 01:07:05 +0100

    Added Map Sum Pairs

---
+++ src/main/java/com/algomind/leetcode/mapsumpairs/MapSum.java
@@ -0,0 +1,40 @@
+package com.algomind.leetcode.mapsumpairs;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class MapSum {
+    private Map<String, Integer> map;
+    private TrieNode root;
+
+    public MapSum() {
+        map = new HashMap<>();
+        root = new TrieNode();
+    }
+
+    public void insert(String key, int val) {
+        int delta = val - map.getOrDefault(key, 0);
+        map.put(key, val);
+        TrieNode cur = root;
+        cur.score += delta;
+        for(char c : key.toCharArray()) {
+            cur.children.putIfAbsent(c, new TrieNode());
+            cur = cur.children.get(c);
+            cur.score += delta;
+        }
+    }
+
+    public int sum(String prefix) {
+        TrieNode cur = root;
+        for(char c : prefix.toCharArray()) {
+            cur = cur.children.get(c);
+            if(cur == null) return 0;
+        }
+        return cur.score;
+    }
+
+    private static class TrieNode {
+        Map<Character, TrieNode> children = new HashMap<>();
+        int score;
+    }
+}

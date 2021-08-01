095151699a031f988318912f29acbcf5ef9fe9ef
Kefeng Zhang <kzhangtech@gmail.com>
2021-08-01 01:05:56 +0100

    Added Binary Tree Longest Consecutive Sequence II

---
+++ src/main/java/com/algomind/leetcode/binarytreelongestconsecutivesequence2/BinaryTreeLongestConsecutiveSequence2.java
@@ -0,0 +1,39 @@
+package com.algomind.leetcode.binarytreelongestconsecutivesequence2;
+
+import com.algomind.leetcode.common.TreeNode;
+
+public class BinaryTreeLongestConsecutiveSequence2 {
+    private int maxLength = 0;
+
+    public int longestConsecutive(TreeNode root) {
+        if(root == null) return 0;
+
+        doSearch(root);
+        return maxLength;
+    }
+
+    private int[] doSearch(TreeNode root) {
+        int inc = 1, dec = 1;
+
+        if(root.left != null) {
+            int[] leftResult = doSearch(root.left);
+            if(root.left.val == root.val + 1) {
+                inc = leftResult[0] + 1;
+            }else if(root.left.val == root.val - 1) {
+                dec = leftResult[1] + 1;
+            }
+        }
+
+        if(root.right != null) {
+            int[] rightResult = doSearch(root.right);
+            if(root.right.val == root.val + 1) {
+                inc = Math.max(inc, rightResult[0] + 1);
+            }else if(root.right.val == root.val - 1) {
+                dec = Math.max(dec, rightResult[1] + 1);
+            }
+        }
+
+        maxLength = Math.max(maxLength, inc + dec - 1);
+        return new int[]{inc, dec};
+    }
+}

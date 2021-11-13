ff45200357a968ddb3164a60451fd45eb962acd1
Kefeng Zhang <kzhangtech@gmail.com>
2021-11-13 17:35:06 +0000

    Added Implement strStr

---
+++ src/test/java/com/algomind/leetcode/implementstrstr/ImplementStrStrTest.java
@@ -0,0 +1,34 @@
+package com.algomind.leetcode.implementstrstr;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+class ImplementStrStrTest {
+    private final ImplementStrStr implementStrStr = new ImplementStrStr();
+
+    @ParameterizedTest
+    @MethodSource("provideData")
+    public void test(int expected, String hayStack, String needle) {
+        var result = implementStrStr.strStr2(hayStack, needle);
+        Assertions.assertEquals(expected, result);
+    }
+
+    private static Stream<Arguments> provideData() {
+        return Stream.of(
+            Arguments.of(2, "hello", "ll"),
+            Arguments.of(-1, "aaaaa", "bba"),
+            Arguments.of(1, "cabcabcd", "abc"),
+            Arguments.of(0, "abcabcd", "abc"),
+            Arguments.of(-1, "aaa", "aaaa"),
+            Arguments.of(0, "aaa", "aaa"),
+            Arguments.of(4, "mississippi", "issip"),
+            Arguments.of(-1, "mississippi", "issipi"),
+            Arguments.of(11, "aabcdeafghiajtjtjklmf", "ajtj"),
+            Arguments.of(0, "", "")
+        );
+    }
+}

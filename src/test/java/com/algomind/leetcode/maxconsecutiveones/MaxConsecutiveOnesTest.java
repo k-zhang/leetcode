85aa647dcb691167dd65dcf2e9040c0cacccc9ae
Kefeng Zhang <kzhangtech@gmail.com>
2022-01-02 21:17:47 +0000

    Added Max Consecutive Ones

---
+++ src/test/java/com/algomind/leetcode/maxconsecutiveones/MaxConsecutiveOnesTest.java
@@ -0,0 +1,30 @@
+package com.algomind.leetcode.maxconsecutiveones;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+class MaxConsecutiveOnesTest {
+    private final MaxConsecutiveOnes solution = new MaxConsecutiveOnes();
+
+    @ParameterizedTest
+    @MethodSource("provideData")
+    public void test(int expected, int[] nums) {
+        Assertions.assertEquals(expected, solution.findMaxConsecutiveOnes(nums));
+    }
+
+    private static Stream<Arguments> provideData() {
+        return Stream.of(
+            Arguments.of(4, new int[]{1,1,1,0,0,0,1,1,1,1,0}),
+            Arguments.of(4, new int[]{0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1}),
+            Arguments.of(1, new int[]{1}),
+            Arguments.of(0, new int[]{0}),
+            Arguments.of(1, new int[]{1,0,0,1}),
+            Arguments.of(4, new int[]{1,1,1,1}),
+            Arguments.of(0, new int[]{0,0,0,0})
+        );
+    }
+}

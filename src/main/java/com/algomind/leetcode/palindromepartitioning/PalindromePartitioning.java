af4c31391c4ec2283bc1eed1c2b420e984ddef60
Kefeng Zhang <kzhangtech@gmail.com>
2022-01-05 22:59:07 +0000

    Added Palindrome Partitioning

---
+++ src/main/java/com/algomind/leetcode/palindromepartitioning/PalindromePartitioning.java
@@ -0,0 +1,59 @@
+package com.algomind.leetcode.palindromepartitioning;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+public class PalindromePartitioning {
+    public List<List<String>> partition(String s) {
+        int len = s.length();
+        if(len == 1) return List.of(List.of(s));
+
+        var dp = generateDP(s, len);
+
+        List<List<String>> res = new ArrayList<>();
+        Deque<String> stack = new ArrayDeque<>();
+        dfs(s, 0, len, dp, stack, res);
+        return res;
+    }
+
+    private void dfs(String s, int index, int len, boolean[][] dp, Deque<String> path, List<List<String>> res) {
+        if (index == len) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+
+        for (int i = index; i < len; i++) {
+            if (dp[index][i]) {
+                path.addLast(s.substring(index, i + 1));
+                dfs(s, i + 1, len, dp, path, res);
+                path.removeLast();
+            }
+        }
+    }
+
+    private boolean[][] generateDP(String s, int len) {
+        boolean[][] dp = new boolean[len][len];
+
+        for (int i = 0; i < len; i++) {
+            dp[i][i] = true;
+        }
+
+        for (int j = 1; j < len; j++) {
+            for (int i = 0; i < j; i++) {
+                if (s.charAt(i) != s.charAt(j)) {
+                    dp[i][j] = false;
+                } else {
+                    if (j - i < 3) {
+                        dp[i][j] = true;
+                    } else {
+                        dp[i][j] = dp[i + 1][j - 1];
+                    }
+                }
+            }
+        }
+
+        return dp;
+    }
+}
---
+++ src/test/java/com/algomind/leetcode/palindromepartitioning/PalindromePartitioningTest.java
@@ -0,0 +1,31 @@
+package com.algomind.leetcode.palindromepartitioning;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Stream;
+
+class PalindromePartitioningTest {
+    private final PalindromePartitioning solution = new PalindromePartitioning();
+
+    @ParameterizedTest
+    @MethodSource("provideData")
+    public void testPartitionArrayForMaximumSum(Set<List<String>> expected, String s) {
+        var result = solution.partition(s);
+        var resultSet = new HashSet<>(result);
+        Assertions.assertEquals(expected, resultSet);
+    }
+
+    private static Stream<Arguments> provideData() {
+        return Stream.of(
+                Arguments.of(Set.of(List.of("a","a","b"),List.of("aa","b")), "aab"),
+                Arguments.of(Set.of(List.of("b","a","b","a","d","a"),List.of("bab","a","d","a"),List.of("b","aba","d","a"),List.of("bab","ada"),List.of("b","a","b","ada")), "babada"),
+                Arguments.of(Set.of(List.of("a")), "a")
+        );
+    }
+}
